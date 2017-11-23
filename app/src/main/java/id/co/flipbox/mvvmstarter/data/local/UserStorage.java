51b88245759f10d6e9de940f4e989ef763686c8e
Aqid <aqid@flipbox.co.id>
2017-11-23 16:26:16 +0700

    [update] fix contracts & maybe pattern

--- app/src/main/java/id/co/flipbox/mvvmstarter/data/local/UserStorage.java
+++ app/src/main/java/id/co/flipbox/mvvmstarter/data/local/UserStorage.java
@@ -5,8 +5,8 @@ import com.orhanobut.hawk.Hawk;
 import java.util.ArrayList;
 import java.util.List;
 
-import id.co.flipbox.mvvmstarter.data.local.contracts.AGERContract;
 import id.co.flipbox.mvvmstarter.data.local.contracts.CacheContract;
+import id.co.flipbox.mvvmstarter.data.local.contracts.RAGEContract;
 import id.co.flipbox.mvvmstarter.models.User;
 import id.co.flipbox.mvvmstarter.utils.constants.K;
 import io.reactivex.Maybe;
@@ -16,13 +16,13 @@ import io.reactivex.schedulers.Schedulers;
  * Created by bukhoriaqid on 5/27/17.
  */
 
-public class UserStorage implements AGERContract<User, Integer>, CacheContract
+public class UserStorage implements RAGEContract<User, Integer>, CacheContract
 {
     @Override
     public boolean isCacheValid ()
     {
         // TODO: 7/28/17 define your own cache validity
-        return true;
+        return false;
     }
 
 
@@ -30,13 +30,28 @@ public class UserStorage implements AGERContract<User, Integer>, CacheContract
     public Maybe<List<User>> getList ()
     {
         List<User> users = isCacheValid() ? Hawk.get(K.USER_LIST, new ArrayList<User>()) : null;
-        return Maybe.just(users).subscribeOn(Schedulers.io());
+        if (users == null)
+        {
+            return Maybe.empty();
+        }
+        else
+        {
+            return Maybe.just(users).subscribeOn(Schedulers.io());
+        }
     }
 
     @Override
     public Maybe<User> get (Integer id)
     {
-        return Maybe.just(isCacheValid() ? Hawk.get(String.format(K.USER_DETAIL, id), new User(0, "", "")) : null);
+        User user = isCacheValid() ? Hawk.get(String.format(K.USER_DETAIL, id), new User(0, "", "")) : null;
+        if (user == null)
+        {
+            return Maybe.empty();
+        }
+        else
+        {
+            return Maybe.just(user).subscribeOn(Schedulers.io());
+        }
     }
 
     @Override
